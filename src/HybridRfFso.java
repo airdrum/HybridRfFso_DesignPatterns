12735e447bb7872888669f47e0c6add0196b672f
Air Drummer <sametyildiz1989@gmail.com>
2019-03-06 01:16:45 +0300

    ConfigFile parser classes are modified and every TEstLog is created inside TestLogManager class. Next step should be creating the related test strings accordingly.

--- src/HybridRfFso.java
+++ src/HybridRfFso.java
@@ -1,48 +1,17 @@
-
-
 import java.util.ArrayList;
 
 import org.json.JSONObject;
-
-import nodefactory.FsoNodeFactory;
-import nodefactory.NodeFactory;
-import nodefactory.RemoteNode;
-import nodefactory.RfFsoNodeFactory;
-import nodefactory.RfNodeFactory;
 
 public class HybridRfFso {
 	
 	public static void main(String[] args) {
 		// TODO Auto-grenerated method stu
-		NodeFactory myRfNodeFactory,myFsoNodeFactory, myRfFsoNodeFactory;
-		RemoteNode fsoNodeSettings, rfNodeSettings, rfFsoNodeSettings;
-		myFsoNodeFactory = new FsoNodeFactory();
-		myRfNodeFactory = new RfNodeFactory();
-		myRfFsoNodeFactory = new RfFsoNodeFactory();
-		JSONObject fsoNode, rfNode,rfFsoNode = null;
+		
 		TestLogManager tManager = new TestLogManager();
-		ArrayList<JSONObject> testItems = tManager.getTestItems();
-		ArrayList<JSONObject> testSettingNodes = new ArrayList<JSONObject>();
-		for (int i = 0; i < testItems.size(); i++) {
-			if (testItems.get(i).get("TransmissionType").equals("FSO")) {
-				fsoNodeSettings = myFsoNodeFactory.createNode((String)testItems.get(i).get("TransmissionDirection"), (String)testItems.get(i).get("TransmissionProtocol"));
-				fsoNode = fsoNodeSettings.getNodeObject();
-				testSettingNodes.add(i, fsoNode);
-			}else if(testItems.get(i).get("TransmissionType").equals("RF")) {
-				rfNodeSettings = myRfNodeFactory.createNode((String)testItems.get(i).get("TransmissionDirection"), (String)testItems.get(i).get("TransmissionProtocol"));
-				rfNode = rfNodeSettings.getNodeObject();
-				testSettingNodes.add(i, rfNode);
-			}else if(testItems.get(i).get("TransmissionType").equals("RFFSO")) {
-				rfFsoNodeSettings = myRfFsoNodeFactory.createNode((String)testItems.get(i).get("TransmissionDirection"), (String)testItems.get(i).get("TransmissionProtocol"));
-				rfFsoNode = rfFsoNodeSettings.getNodeObject();
-				testSettingNodes.add(i, rfFsoNode);
-			}
-			
-		}
-		System.out.println("TEST ITEMS:"+testItems);
-
-		System.out.println("TEST SETTINGS:"+testSettingNodes);
-
+		ArrayList<JSONObject> settings = tManager.getTestSettings();
+		ArrayList<JSONObject> items = tManager.getTestItems();
+		System.out.println("ITEMS:" + items);
+		System.out.println("SETTINGS:" + settings);
 
 	}
 
--- src/TestLogManager.java
+++ src/TestLogManager.java
@@ -2,9 +2,16 @@ import java.util.ArrayList;
 
 import org.json.*;
 import config.TestConfig;
+import nodefactory.FsoNodeFactory;
+import nodefactory.NodeFactory;
+import nodefactory.RemoteNode;
+import nodefactory.RfFsoNodeFactory;
+import nodefactory.RfNodeFactory;
 public class TestLogManager {
 	private ArrayList<JSONObject> testItems = null;
-	
+	private ArrayList<JSONObject> testSettingNodes = null;
+	// TODO Auto-grenerated method stu
+			
 	public TestLogManager() {
 		
 		JSONObject testSettings = null;
@@ -24,4 +31,34 @@ public class TestLogManager {
 		return this.testItems;
 	}
 
+	private void setTestSettings(){
+		NodeFactory myRfNodeFactory,myFsoNodeFactory, myRfFsoNodeFactory;
+		RemoteNode fsoNodeSettings, rfNodeSettings, rfFsoNodeSettings;
+		myFsoNodeFactory = new FsoNodeFactory();
+		myRfNodeFactory = new RfNodeFactory();
+		myRfFsoNodeFactory = new RfFsoNodeFactory();
+		JSONObject fsoNode, rfNode,rfFsoNode = null;
+		testSettingNodes = new ArrayList<JSONObject>();
+		for (int i = 0; i < this.testItems.size(); i++) {
+			if (this.testItems.get(i).get("TransmissionType").equals("FSO")) {
+				fsoNodeSettings = myFsoNodeFactory.createNode((String)this.testItems.get(i).get("TransmissionDirection"), (String)this.testItems.get(i).get("TransmissionProtocol"));
+				fsoNode = fsoNodeSettings.getNodeObject();
+				this.testSettingNodes.add(i, fsoNode);
+			}else if(this.testItems.get(i).get("TransmissionType").equals("RF")) {
+				rfNodeSettings = myRfNodeFactory.createNode((String)this.testItems.get(i).get("TransmissionDirection"), (String)this.testItems.get(i).get("TransmissionProtocol"));
+				rfNode = rfNodeSettings.getNodeObject();
+				this.testSettingNodes.add(i, rfNode);
+			}else if(this.testItems.get(i).get("TransmissionType").equals("RFFSO")) {
+				rfFsoNodeSettings = myRfFsoNodeFactory.createNode((String)this.testItems.get(i).get("TransmissionDirection"), (String)this.testItems.get(i).get("TransmissionProtocol"));
+				rfFsoNode = rfFsoNodeSettings.getNodeObject();
+				this.testSettingNodes.add(i, rfFsoNode);
+			}	
+		}		
+	}
+	
+	public ArrayList<JSONObject> getTestSettings(){
+		setTestSettings();
+		return this.testSettingNodes;
+	}
+	
 }
